42291073ec62085e1b0bf7fcc47e89364354147c
Rasmus Rønn Nielsen <rasmusrnielsen@gmail.com>
2015-04-18 18:22:05 +0200

    Should've been in last commit

--- rendering.js
+++ rendering.js
@@ -1,40 +1,9 @@
 (function() {
-  var materials = {};
-  var geometries = {};
-  var components = [];
-
-  function init() {
-    materials.projectile = new THREE.MeshLambertMaterial({
-      color: 0xcc0000,
-      shading: THREE.FlatShading
-    });
-
-    var segments = 14, rings = 6;
-    geometries.projectile = new THREE.SphereGeometry(Config.projectileRadius, segments, rings);
-  }
-
-  function create(geometryName, materialName) {
-    var geometry = geometries[geometryName];
-    if(!geometry) throw new Error("Geometry not found.");
-
-    var material = materials[materialName];
-    if(!material) throw new Error("Material not found.");
-
-    var mesh = new THREE.Mesh(geometry, material)
-    Game.scene.add(mesh);
-
-    components.push(mesh);
-
-    return components.length-1;
-  }
-
-  function get(handle) {
-    return components[handle];
+  function add(object) {
+    Game.scene.add(object);
   }
 
   window.Rendering = {
-    init: init,
-    create: create,
-    get: get
+    add: add
   };
 })();
